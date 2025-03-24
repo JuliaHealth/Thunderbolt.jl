64a247cf47d23342bcb76cc2f9e930930a6937b7
Abdelrahman95 <abdelrahmanfathe95@gmail.com>
2025-03-25 00:12:00 +0200

    minor change

--- test/gpu/test_preconditioners.jl
+++ test/gpu/test_preconditioners.jl
@@ -3,11 +3,11 @@ using CUDA
 using Thunderbolt
 using LinearSolve
 
-N = 4
+N = 8
 A = spdiagm(0 => 2 * ones(N), -1 => -ones(N-1), 1 => -ones(N-1))
 
 cudal1prec=  Thunderbolt.CudaL1PrecBuilder()
-P = cudal1prec(A; n_threads=1, n_blocks=1)
+P = cudal1prec(A; n_threads=2, n_blocks=1)
 x = eltype(A).(collect(0:N-1))
 y = x
 LinearSolve.ldiv!(y, P, x)
