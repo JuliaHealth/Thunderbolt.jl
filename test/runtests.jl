ef863d8c57ff844b0afb0f43840633d24b96fc8c
termi-official <termi-official@users.noreply.github.com>
2025-04-03 00:21:42 +0200

    Merge solid tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -71,7 +71,6 @@ include("test_mesh.jl")
 include("test_coefficients.jl")
 include("test_microstructures.jl")
 
-include("integration/test_passive_structure.jl")
 include("integration/test_solid_mechanics.jl")
 include("integration/test_electrophysiology.jl")
 include("integration/test_ecg.jl")
