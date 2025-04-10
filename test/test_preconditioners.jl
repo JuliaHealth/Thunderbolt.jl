0794dce43e4644055926475a052a5a86a33ea0ef
Abdelrahman95 <abdelrahmanfathe95@gmail.com>
2025-04-10 21:20:15 +0200

    rm direct solver

--- test/test_preconditioners.jl
+++ test/test_preconditioners.jl
@@ -46,8 +46,6 @@ function test_sym_csr(A,x,partsize,backend)
         N = size(A, 1)
         for nparts in 1:partsize:N
             P = builder(B, partsize, nparts)
-            # y = similar(x)
-            # LinearSolve.ldiv!(y, P, x)
             y = P \ x
             @test isapprox(y, expected_y; atol=1e-10)
         end
@@ -60,22 +58,19 @@ function test_l1gs_prec(A,b,test_set_name)
         nparts = ThreadPinning.ncores()
         partsize = size(A,1) / nparts |> ceil |> Int
     
-        u = A\b
-    
         prob = LinearProblem(A, b)
         sol_unprec = solve(prob, KrylovJL_GMRES())
-        # check both iterative solution and direct solution
-        @test sol_unprec.u ≈ u
+        @test isapprox(A * sol_unprec.u,b,rtol=1e-1, atol=1e-1)
     
         # Test L1GS Preconditioner
         P = L1GSPrecBuilder(CPU())(A, partsize, nparts)
         sol_prec = solve(prob, KrylovJL_GMRES(P);Pl=P)
         println( "Unprec. no. iters: $(sol_unprec.iters), time: $(sol_unprec.stats.timer)")
         println( "Prec. no. iters: $(sol_prec.iters), time: $(sol_prec.stats.timer)")
-        @test isapprox(sol_prec.u , u, atol=1e-2)
+        @test isapprox(A * sol_prec.u , b,rtol=1e-1, atol=1e-1)
         @test sol_prec.iters < sol_unprec.iters
         @test sol_prec.resid < sol_unprec.resid
-        @test sol_prec.stats.timer <= sol_unprec.stats.timer 
+        @test sol_prec.stats.timer < sol_unprec.stats.timer 
     end
 end
 
@@ -95,10 +90,7 @@ end
             expected_y2 = poisson_l1gs_expected_result(x)  
             expected_y2[2] = x[2] / (A2[2,2] + abs(A2[2,3]) + abs(A2[2,8]) )  # Adjusted for non-symmetric case
 
-            # CPU
             P_cpu = L1GSPrecBuilder(CPU())(A2, 2, 1)
-            # y_cpu = similar(x)
-            # LinearSolve.ldiv!(y_cpu, P_cpu, x)
             y_cpu = P_cpu \ x
             @test isapprox(y_cpu, expected_y2; atol=1e-10)
         end
@@ -114,7 +106,7 @@ end
         test_l1gs_prec(A,b,test_set_name)
         
         test_set_name = "Symmetric A"
-        md = mdopen("HB/bcsstk15") 
+        md = mdopen("HB/bcsstk15") # ill-conditioned matrix
         A = md.A
         b = ones(size(A,1))
         test_l1gs_prec(A,b,test_set_name)
