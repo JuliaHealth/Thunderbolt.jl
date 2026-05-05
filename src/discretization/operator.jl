fe3e7e280f9a24fbf9dc4f7bc6b7b18c42662446
termi-official <9196588+termi-official@users.noreply.github.com>
2026-05-05 19:43:36 +0200

    Remove unused method

--- src/discretization/operator.jl
+++ src/discretization/operator.jl
@@ -1,33 +1,3 @@
-# This function is defined to make things sufficiently type-stable
-function _sequential_update_linearization_on_subdomain_Jr!(
-    assembler,
-    sdh,
-    element_cache,
-    facet_cache,
-    tying_cache,
-    u,
-    time,
-)
-    # Prepare standard values
-    ndofs = ndofs_per_cell(sdh)
-    Jₑ = zeros(ndofs, ndofs)
-    uₑ = zeros(ndofs)
-    rₑ = zeros(ndofs)
-    uₜ = get_tying_dofs(tying_cache, u)
-    @inbounds for cell in CellIterator(sdh)
-        fill!(Jₑ, 0)
-        fill!(rₑ, 0)
-        dofs = celldofs(cell)
-
-        uₑ .= @view u[dofs]
-        @timeit_debug "assemble element" assemble_element!(Jₑ, rₑ, uₑ, cell, element_cache, time)
-        # TODO maybe it makes sense to merge this into the element routine in a modular fasion?
-        # TODO benchmark against putting this into the FacetIterator
-        @timeit_debug "assemble facets" assemble_element!(Jₑ, rₑ, uₑ, cell, facet_cache, time)
-        @timeit_debug "assemble tying" assemble_tying!(Jₑ, rₑ, uₑ, uₜ, cell, tying_cache, time)
-        assemble!(assembler, dofs, Jₑ, rₑ)
-    end
-end
 
 function needs_update(op::LinearFerriteOperator, t)
     return _needs_update(op, op.integrator.integrand, t)
