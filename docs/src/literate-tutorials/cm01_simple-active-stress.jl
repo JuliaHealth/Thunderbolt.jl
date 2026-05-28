ab8e085a4c3f4a33e38947d45c2bcca55791d9aa
termi-official <termi-official@users.noreply.github.com>
2026-05-28 18:47:02 +0200

    Revert tutorial 1

--- docs/src/literate-tutorials/cm01_simple-active-stress.jl
+++ docs/src/literate-tutorials/cm01_simple-active-stress.jl
@@ -15,24 +15,13 @@
 # According to [Cha:1982:mlv](@citet) the additive split of the stress in active and passive parts dates back to unpublished Peskin and has been popularized by [GucWalMcC:1993:mac](@citet).
 #
 # ## Commented Program
-# We start by loading Thunderbolt, LinearSolve, and FerriteMultigrid.
+# We start by loading Thunderbolt and LinearSolve to use a custom direct solver of our choice.
 using Thunderbolt, LinearSolve
-using FerriteMultigrid: FerriteMultigrid, GridHierarchy, GaussSeidel
-using IterativeSolvers: gauss_seidel!
-using AlgebraicMultigrid
-using TimerOutputs
-import LinearAlgebra: diag, mul!
-using HybridSmoothers
-import KernelAbstractions as KA
-
-TimerOutputs.enable_debug_timings(AlgebraicMultigrid)
-TimerOutputs.enable_debug_timings(FerriteMultigrid)
-reset_timer!()
 
 # Our goal is to simulate the contraction of a left ventricle with a very simple active stress formulation.
 # Hence in a first step we need to load a suitable mesh.
 # Thunderbolt can generate idealized geometries as follows.
-mesh = generate_ideal_lv_mesh(6,2,3;
+mesh = generate_ideal_lv_mesh(11,2,5;
     inner_radius = 0.7,
     outer_radius = 1.0,
     longitudinal_upper = 0.2,
@@ -48,15 +37,9 @@ mesh = generate_ideal_lv_mesh(6,2,3;
 #     We can also load realistic geometries with external formats. For this simply use either FerriteGmsh.jl
 #     or one of the loader functions stated in the [mesh API](@ref mesh-utility-api).
 
-# We now build a two-level grid hierarchy for geometric multigrid.
-# The hexahedralized mesh is the coarse level; one uniform refinement produces the fine level
-# on which the simulation will actually run.
-gh = GridHierarchy(mesh, 1)
-fine_mesh = gh.grids[end]
-
 # Next we will define a coordinate system, which helps us to work with cardiac geometries.
 # This way we can reuse different methods, like for example fiber generators, across geometries.
-coordinate_system = compute_lv_coordinate_system(fine_mesh);
+coordinate_system = compute_lv_coordinate_system(mesh);
 
 # In this coordinate system we will now create a microstructure with linearly varying helix angle in transmural direction.
 # The compute microstructure field will be generated on the function space of piecewise continuous first order Lagrange polynomials.
@@ -107,7 +90,7 @@ active_stress_model = ActiveStressModel(
 
 # Next we define some boundary conditions.
 # In order to have a very rough approximation of the effect of the pericardium, we use a Robin boundary condition in normal direction.
-weak_boundary_conditions = (RobinBC(10.0, "Epicardium"),)
+weak_boundary_conditions = (NormalSpringBC(1.0, "Epicardium"),)
 
 # We finalize the mechanical model by assigning a symbol to identify the unknown solution field and connect the active stress model with the weak boundary conditions.
 mechanical_model = QuasiStaticModel(:displacement, active_stress_model, weak_boundary_conditions)
@@ -117,14 +100,13 @@ mechanical_model = QuasiStaticModel(:displacement, active_stress_model, weak_bou
 
 # We now need to transform the space-time problem into a time-dependent problem by discretizing it spatially.
 # This can be accomplished by the function semidiscretize, which takes a model and the disretization technique.
-# We use second-order Lagrange polynomials for the displacement field, which is required for polynomial multigrid
-# (p-MG coarsens from order 2 to order 1).
+# Here we use a finite element discretization in space with first order Lagrange polynomials to discretize the displacement field.
 # !!! danger
 #     The discretization API does now play well with multiple domains right now and will be updated with a possible breaking change in future releases.
 spatial_discretization_method = FiniteElementDiscretization(
-    Dict(:displacement => LagrangeCollection{2}()^3),
+    Dict(:displacement => LagrangeCollection{1}()^3),
 )
-quasistaticform = semidiscretize(mechanical_model, spatial_discretization_method, fine_mesh);
+quasistaticform = semidiscretize(mechanical_model, spatial_discretization_method, mesh);
 
 # The remaining code is very similar to how we use SciML solvers.
 # We first define our time domain, initial time step length and some dt for visualization.
@@ -140,41 +122,17 @@ problem = QuasiStaticProblem(quasistaticform, tspan);
 
 # Next we define the time stepper.
 # Since there are no time derivatives appearing in our formulation we have to opt for a homotopy path method, which solve the time depentent problems adaptively.
-# As our non-linear solver we choose the standard Newton-Raphson method.
-# For the linear system inside each Newton step we use chained polynomial + geometric multigrid as preconditioner.
-# The polynomial hierarchy coarsens from p=2 to p=1; the geometric hierarchy then coarsens
-# the p=1 problem using the grid levels in `gh`.  Both DofHandler hierarchies are assembled automatically.
+# As our non-linear solver we choose the standard Newton-Raphson method and a direct solver for the inner linear system.
 # For the theory behind homotopy path methods we refer to [the corresponding theory manual on homotopy path methods](@ref theory_homotopy-path-methods)
 timestepper = HomotopyPathSolver(
     NewtonRaphsonSolver(
-        max_iter     = 10,
-        inner_solver = KrylovMGSolver(
-            KrylovJL_GMRES(; verbose = 1),
-            ChainedMGPrecon(
-                PMGPrecon(;
-                    presmoother  = AlgebraicMultigrid.SOR(1.3, AlgebraicMultigrid.ForwardSweep(), 2),
-                    postsmoother = AlgebraicMultigrid.SOR(1.3, AlgebraicMultigrid.BackwardSweep(), 2),
-                ),
-                GMGPrecon(gh;
-                    presmoother  = AlgebraicMultigrid.SOR(1.3, AlgebraicMultigrid.ForwardSweep(), 2),
-                    postsmoother = AlgebraicMultigrid.SOR(1.3, AlgebraicMultigrid.BackwardSweep(), 2),
-                ),
-            );
-            maxiters = 100,
-        ),
-        ## inner_solver = LinearSolve.UMFPACKFactorization(),
-        forcing = EisenstatWalkerForcing(ηₘₐₓ = 0.9),
+        max_iter=10,
+        inner_solver=LinearSolve.UMFPACKFactorization(),
     )
 );
 
-# !!! tip
-#     For quick experiments or when FerriteMultigrid is not available, replace `KrylovMGSolver`
-#     with a direct solver by uncommenting `inner_solver = LinearSolve.UMFPACKFactorization()`
-#     above (and removing the `KrylovMGSolver` block).
-
 # Now we initialize our time integrator as usual.
 integrator = init(problem, timestepper, dt=dt₀, verbose=true, adaptive=true, dtmax=25.0);
-@info length(integrator.u)
 
 # !!! todo
 #     The post-processing API is not yet finished.
@@ -190,8 +148,6 @@ for (u, t) in TimeChoiceIterator(integrator, tspan[1]:dtvis:tspan[2])
     Thunderbolt.store_timestep_field!(io, t, dh, u, :displacement)
     end
 end;
-
-print_timer()
 
 # !!! tip
 #     If you want to see more details of the solution process launch Julia with Thunderbolt as debug module:
