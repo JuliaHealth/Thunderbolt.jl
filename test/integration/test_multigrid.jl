ab029d90df43ebdbbd6c5ce45e7f5e3e8f47511b
termi-official <termi-official@users.noreply.github.com>
2026-06-17 14:46:02 +0200

    Format test

--- test/integration/test_multigrid.jl
+++ test/integration/test_multigrid.jl
@@ -1,8 +1,8 @@
 
 module TestMultigridHowto
-    mktempdir() do dir
-        cd(dir) do
-            include(joinpath(@__DIR__, "../../docs/src/literate-howto/multigrid.jl"))
-        end
+mktempdir() do dir
+    cd(dir) do
+        include(joinpath(@__DIR__, "../../docs/src/literate-howto/multigrid.jl"))
     end
+end
 end
