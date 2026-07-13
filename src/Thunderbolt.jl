136f639edddd9e1f0e6ba46baa1368165dde1ed7
Abdulaziz <Abdulaziz.Mohamed@edu.ruhr-uni-bochum.de>
2026-07-13 20:39:24 +0200

    CI

--- src/Thunderbolt.jl
+++ src/Thunderbolt.jl
@@ -100,7 +100,7 @@ import SymbolicIndexingInterface
 import SciMLBase
 @reexport import SciMLBase: init, solve, solve!, step!
 @reexport import SciMLIterators: TimeChoiceIterator
-using SciMLBase: recursivecopy!, recursivecopy
+using RecursiveArrayTools: recursivecopy!, recursivecopy
 import DiffEqBase#: AbstractDiffEqFunction, AbstractDEProblem
 import OrdinaryDiffEqCore#: OrdinaryDiffEqCore
 import OrdinaryDiffEqCore:
